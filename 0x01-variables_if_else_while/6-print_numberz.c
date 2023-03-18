3fb7d7dd7b26d2bb2d3b5a8d1e6e0af63d6bdf14
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-03-18 00:13:13 +0000

    prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * @void: indicate no parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}

aef3e62bad9720869552fae0ad92acbe2a23aa36
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-05-29 17:36:32 +0000

    function that prints every other character of a string, starting with the first character, followed by a new line.

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -1,7 +1,9 @@
-#include "main.h"
+#include <stdio.h>
+
+void puts2(char *);
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -9,7 +11,7 @@ int main(void)
 {
 	char *str;
 
-	str = "0123456789";
+	str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\0Holberton";
 	puts2(str);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,11 +12,13 @@
 
 void puts2(char *str)
 {
-	int i;
+	int index = 0, len = 0;
+	
+	while (str[index++])
+		len++;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-	{
-		_putchar(str[i]);
-	}
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
 	_putchar('\n');
 }

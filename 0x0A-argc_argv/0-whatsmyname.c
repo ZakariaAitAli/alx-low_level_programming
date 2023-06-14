a4101cbe2cfa3cc74c7c24950ceb9f9517a37d43
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-06-14 22:39:45 +0000

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program that prints its name'
+ * @argc: number of argument
+ * @argv: array of pointers to the args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

001a667fb2ecfd7ec0b73bc52e27a8d5e9570e62
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-06-20 23:04:19 +0100

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - dog details structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * description: none
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

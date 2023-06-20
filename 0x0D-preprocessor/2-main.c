ca5b0159c43968bb8af13052bdc52a6570b6d01b
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-06-20 22:33:07 +0100

    program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints the name of the file of the program'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

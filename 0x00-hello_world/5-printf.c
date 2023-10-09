c2951b81966e5aa4d24e64749de530cf3a318248
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-09 19:56:54 +0000

    C program that prints exactly with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

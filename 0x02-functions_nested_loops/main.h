855d90def67b8809316e20bd5c84b68c3ab6f426
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-10 23:19:17 +0000

    program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+
+#endif

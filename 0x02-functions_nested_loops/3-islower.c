5eec91aa4b9faa5491e0f595735b859ff600fd84
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-11 00:59:10 +0100

    Checks if a character is lowercase.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-islower.c
+ * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}

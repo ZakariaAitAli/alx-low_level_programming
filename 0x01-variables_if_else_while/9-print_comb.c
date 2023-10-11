567f5cf2972ea55b13d2fdd7319301d135b20e03
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-11 13:15:12 +0100

    Prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}

2116a0699c6bc6d22195c7a317f7ee5fd0aca2bb
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-11 02:39:19 +0100

    Prints all natural numbers from input to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
+

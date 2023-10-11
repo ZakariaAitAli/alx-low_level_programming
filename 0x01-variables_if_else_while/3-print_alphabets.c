6c5833405e02f94b371f5737162ed21ecfb07f1f
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-11 05:00:32 +0100

    Prints the alphabet in lowercase, and then in uppercase.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}

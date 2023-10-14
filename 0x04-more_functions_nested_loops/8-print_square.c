95b769099864a33c77e34fe2eff527db469cbe19
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-14 21:51:47 +0100

    Prints a square using the character #.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+/*
+ * File: 8-print_square.c
+ * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
+ *
+ */
+
+#include "main.h"
+
+/**
+ * print_square - Prints a squareusing the character #.
+ * @size: The size of the square.
+ */
+
+void print_square(int size)
+{
+	int hgt, wid;
+
+	if (size > 0)
+	{
+		for (hgt = 0; hgt < size; hgt++)
+		{
+			for (wid = 0; wid < size; wid++)
+				_putchar('#');
+
+			if (hgt == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}

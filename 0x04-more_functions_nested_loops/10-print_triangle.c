7144613251819c7ca1a5275967842add0254095f
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-18 16:05:54 +0100

    function that prints a triangle, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+/*
+ * File: 10-print_triangle.c
+ * Auth: Zakaria AIT ALI [zakariaaitali555@gmail.com]
+ */
+
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, using the character #.
+ * @size: The size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int hash, index;
+
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+				_putchar(' ');
+
+			for (index = 0; index < hash; index++)
+				_putchar('#');
+
+			if (hash == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}

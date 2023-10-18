b27b54e51b4f1917b79b2d02889ab0ac537009fb
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-18 16:34:13 +0100

    program that finds and prints the largest prime factor of the number 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+/*
+ * File: 100-prime_factor.c
+ * Auth: Zakaria AIT ALI [zakariaaitali555@gmail.com]
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the largest prime
+ *        factor of the number 612852475143.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long prime = 612852475143, div;
+
+	while (div < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+
+		for (div = 3; div < (prime / 2); div += 2)
+		{
+			if ((prime % div) == 0)
+				prime /= div;
+		}
+	}
+
+	printf("%ld\n", prime);
+
+	return (0);
+}

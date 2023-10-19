00997a1ee20ddf48ff6793a64ce42f8d93ad4d1c
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-19 01:01:14 +0100

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+/*
+ * File: 5-rev_string.c
+ * Auth: Zakaria AIT ALI [zakariaaitali555@gmail.com]
+ *
+ */
+
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char tmp;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ int _strlen(char *s);
 int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif

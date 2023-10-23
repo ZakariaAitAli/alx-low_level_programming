6509a51dcf202bc0f6aefe3ae99c335048e022fe
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-23 21:10:04 +0100

    Fix Betty style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
 #include <stddef.h>
 
 /**
- * memset - Fills the first n bytes of the memory area
+ * _memset - Fills the first n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled.
  * @b: The character to fill the memory area with.

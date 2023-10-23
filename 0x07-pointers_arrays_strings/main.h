23d0be8a0e893def7085e1b7e41913fccf594e74
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-23 22:06:56 +0100

    function that locates a character in a string.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ int _putchar(char c);
 /* functions */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif

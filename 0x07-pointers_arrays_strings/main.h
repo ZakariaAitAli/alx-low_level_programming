7897167c694392f705c2ffae1b549cd084ba8c05
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-23 21:08:44 +0100

    function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Zakaria Ait Ali [zakariaaitali555@gmail.com]
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x07-pointers_arrays_strings directory.
+ */
+
+
+/* utils */
+int _putchar(char c);
+
+/* functions */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif

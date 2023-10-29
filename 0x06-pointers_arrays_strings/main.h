08a7fcc29f403ff915837a29cbbb30ea56825ae4
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2023-10-29 18:20:46 +0100

    Created main header

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+/* Prototypes */
+char *_strcat(char *dest, char *src);
+
+#endif

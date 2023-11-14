8652f7cd240ce467d297138dd9715ac36f0b9162
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2023-11-14 20:43:25 +0100

    Task 6 : function that returns 1 if the input integer is a prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -18,5 +18,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 #endif /* ALX_LOW_LEVEL_PROGRAMMING_MAIN_H */

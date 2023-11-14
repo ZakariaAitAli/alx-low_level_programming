25fc2c79e4bd522b7a22aa4a876430ef5806f30b
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2023-11-14 22:47:11 +0100

    fix putchar

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,27 +1,13 @@
-/*
- * File: 0-putchar.c
- * Auth: Zakaria Ait Ali <zakariaaitali555@gmail.com>
- *
- */
-
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - Prints "_putchar" followed by a new line.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }

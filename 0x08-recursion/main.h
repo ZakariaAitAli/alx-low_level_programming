a1c3bd3985df2bc400d525298029f5e366ccada9
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-02-06 18:04:56 +0100

    Task 8: function that compares two strings

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -20,5 +20,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* ALX_LOW_LEVEL_PROGRAMMING_MAIN_H */

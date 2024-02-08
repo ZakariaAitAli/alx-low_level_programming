e3ce0283cf26942ab2202ad182bebc64c5304765
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2024-02-08 17:16:20 +0100

    Task 4: program that add two numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+/*
+ * File: 4-add.c
+ * Auth: Zakaria AIT ALI
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the sum of two numbers
+ * @argc: the number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the program the receives two args - 0
+ *		   If does not - 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int firstNumber, secondNumber, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	firstNumber = atoi(argv[1]);
+	secondNumber = atoi(argv[2]);
+	result = firstNumber + secondNumber;
+
+	printf("%d\n", result);
+	return (0);
+}
